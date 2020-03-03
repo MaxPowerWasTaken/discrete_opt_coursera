2e25e7ce4eb9ab044fdaa42c53680bd2f667521c
Max Epstein <mepstein68@gmail.com>
2020-03-03 11:23:50 -0500

    added dynamic programming solution. aces 1-3, takes 3 hours for 4

--- knapsack/solver.py
+++ knapsack/solver.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
 def unzip(iterable):
     ''' thanks to https://stackoverflow.com/a/22115957/1870832'''
@@ -48,16 +49,52 @@ def greedy_select(values, weights, K):
 
     return total_value, item_selections
 
+
+def dp_select(values, weights, capacity):
+    """Use dynamic programming to build up to solution for all items"""
+    
+    # convert params to n,K notation as used in equations in dynamic programming notes
+    n = len(values)
+    K = capacity
+    
+    # calculate table of optimal value by j,k (j items in 0..n, k is all capacities in 0..K)
+    # see 9:00 - 12:30 here: https://www.coursera.org/learn/discrete-optimization/lecture/wFFdN/knapsack-4-dynamic-programming
+    values_table = np.zeros((K+1,n+1), dtype=np.uint32)
+    
+    print("building DP optimal-value table for n,K: ", n, ",", K)
+    for j in tqdm(range(1, n+1)):
+        item_weight = weights[j-1]
+        item_value  = values[j-1]
+        for k in range(1, K+1):
+            if item_weight > k:
+                values_table[k,j] = values_table[k, j-1]
+            else:
+                values_table[k,j] = max(values_table[k, j-1], item_value + values_table[k-item_weight, j-1])
+    optimal_value = values_table[-1, -1]
+    print(f"optimal value is {optimal_value}. Now proceeding to derive final item-set")
+
+    # from this table of optimal values, we now need to derive final item-set for optimal solution
+    # logic of code below explained 12:30 - 14:00 at https://www.coursera.org/learn/discrete-optimization/lecture/wFFdN/knapsack-4-dynamic-programming
+    taken = [0] * len(values)
+    k = K   # in keeping w/ eqs, K is total capacity but k is k'th row as we move through j,k table
+    for j in range(n, 0, -1):
+        if values_table[k,j] != values_table[k,j-1]:
+            taken[j-1] = 1
+            k = k - weights[j-1]
+    
+    return optimal_value, taken
+
+
 def solve_it(input_data):
     
     # parse the input
     (values, weights, K) = parse_data(input_data)
     
     # value-dense greedy algorithm for filling the knapsack
-    obj, item_selections = greedy_select(values, weights, K)
+    obj, item_selections = dp_select(values, weights, K)
 
     # prepare the solution in the specified output format
-    PROVED_OPTIMAL = 0
+    PROVED_OPTIMAL = 1
     output_data = f"{obj} {PROVED_OPTIMAL}\n"
     output_data += ' '.join(str(i) for i in item_selections)
     return output_data
