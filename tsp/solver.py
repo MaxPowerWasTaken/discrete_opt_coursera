bdda68f55a9d99059eeeb5d44fc5f055abfb6585
Max Epstein <mepstein68@gmail.com>
2020-03-24 12:08:48 -0400

    debugging, added a bunch of print statements, something wrong with my exclude list

--- tsp/solver.py
+++ tsp/solver.py
@@ -37,12 +37,18 @@ def greedy_salesman(distance_matrix, startnode=0):
 
 def get_closest_nodes(current_pt, dist_matrix, n, exclude=[]):
     dist_to_alternatives = dist_matrix[current_pt].copy()
+    #if verbose == 1:
+    print(f"dist_to_alternatives before exclusions are: {', '.join([str(d) for d in dist_to_alternatives])}")
+    print(f"exclusions: {exclude}")
     dist_to_alternatives[unique_l(exclude + [current_pt])] = np.inf
     neighbors_idx = np.argpartition(dist_to_alternatives, n)[:n]  # thanks https://stackoverflow.com/a/34226816/1870832
+    #if verbose == 1:
+    print(f"dist_to_alternatives after exclusions are: {', '.join([str(d) for d in dist_to_alternatives])}")
+    print(f"neighbors_idx: {neighbors_idx}")
     return neighbors_idx
 
 
-def heuristic_search_salesman(distance_matrix, startnode=0, n_closest=3, n_levels=3):
+def heuristic_search_salesman(distance_matrix, startnode=0, n_closest=3, n_levels=3, verbose=0):
     '''At each node, consider a few closest next steps, then a few from there, etc.
         - Choose immediate next step which is on the way to best outcome gamed out n steps forward
         - See for ref Sec 8.9 "Reinforcement Learning," by Sutton and Barto
@@ -55,7 +61,7 @@ def heuristic_search_salesman(distance_matrix, startnode=0, n_closest=3, n_level
     n_levels:        BASICALLY HARDCODED FOR NOW Plan this many levels/steps ahead before choosing next step
     drop_worst:      (NOT IMPLEMENTED) If True, do not consider in plan branches off worst node considered at each level 
     '''
-    print(f"Starting Heuristic Search Salesman for n_closest={n_closest} and n_levels={n_levels}")
+    print(f"Starting Heuristic Search Salesman for n_levels={n_levels} & n_closest={n_closest}")
 
     # input validation
 
@@ -72,10 +78,18 @@ def heuristic_search_salesman(distance_matrix, startnode=0, n_closest=3, n_level
 
         for level in range(1, n_levels):
             for candidate in nodes_by_tree_lvl[level-1]:
-                #print(f"calculating for level {level} and candidate {candidate}")
+                if verbose == 1:
+                    print('\n--------------------------------------------------------')
+                    print(f"calculating for level {level} and candidate {candidate}")
+                    print('--------------------------------------------------------')
                 candidate_ancestors = [int(a.name) for a in candidate.ancestors]
                 exclude = unique_l(visit_order + candidate_ancestors)
-                next_candidates = get_closest_nodes(int(candidate.name), distance_matrix, n_closest, exclude=visit_order)
+                if verbose == 1:
+                    print(f"exclude-list for next candidates is: {exclude}")
+                next_candidates = get_closest_nodes(int(candidate.name), distance_matrix, n_closest, exclude=exclude)
+                if verbose == 1:
+                    print(f"next candidates: {next_candidates}")
+                    input("Press Enter to continue...")
                 nodes_by_tree_lvl[level] = nodes_by_tree_lvl[level] + [Node(str(nc), parent=candidate) for nc in next_candidates]
 
         # Now that the heuristic search tree is constructed, calculate full distance for each path,
@@ -83,14 +97,18 @@ def heuristic_search_salesman(distance_matrix, startnode=0, n_closest=3, n_level
         #print(RenderTree(root))
         next_step = np.nan
         shortest_dist = np.inf
+        #distances = pd.DataFrame({'path':[], 'total_distance':[]})
         for possible_path in root.leaves:  # (Node('/0/1/3'), Node('/0/1/4'), Node('/0/2/5'), Node('/0/2/6'))
             nodes = [n.name for n in possible_path.ancestors] + [possible_path.name]
             dist = sum(distance_matrix[int(i),int(j)] for i,j in zip(nodes[0:-1],nodes[1:]))
-            #print(f"distance for {nodes} is {dist}")
+            if verbose==1:
+                #distances = pd.concat([distances, pd.DataFrame({'path'['/'.join(nodes), 'total_distance':[dist]})], axis=0)
+                print(f"distance for {nodes} is {dist}")
             if dist < shortest_dist:
                 shortest_dist = dist
                 next_step = int(nodes[1])  # nodes[1] is second item in list, but first item is current-point
 
+        print(f"next_step is: {next_step}")
         visit_order.append(next_step)
 
     return visit_order
@@ -105,13 +123,15 @@ def calc_tour_dist(tour_order, dist_matrix):
     return total_dist
 
 
-def solve_it(input_data, input_filename, n_starts=10):
+def solve_it(input_data, input_filename, n_levels=1, n_closest=3, n_starts=10, verbose=1):
 
     # Calculate distance matrix & initial route
     distance_matrix = get_dist_matrix(input_data)
+    if verbose ==1:
+        pd.DataFrame(distance_matrix, columns=[[str(i) for i in range(len(distance_matrix))]]).to_csv('distance_matrix.csv')
 
     # Starting towards a better initial solution using heuristic search trees
-    tour = heuristic_search_salesman(distance_matrix, startnode=0, n_closest=2, n_levels=2)  # n_closest=1 would be original greedy (which is 506.36 on prob1)
+    tour = heuristic_search_salesman(distance_matrix, startnode=0, n_closest=n_closest, n_levels=n_levels, verbose=verbose)  # n_closest=1 would be original greedy (which is 506.36 on prob1)
     tour_dist = calc_tour_dist(tour, distance_matrix)
 
 
@@ -143,9 +163,11 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         #file_location = "data/tsp_51_1"
         file_location = sys.argv[1].strip()
+        n_levels = int(sys.argv[2])
+        n_closest = int(sys.argv[3])
         with open(file_location, 'r') as input_data_file:
             input_data = input_data_file.read()
-        print(solve_it(input_data, file_location))
+        print(solve_it(input_data, file_location, n_levels=n_levels, n_closest=n_closest))
     else:
         print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/tsp_51_1)')
 
