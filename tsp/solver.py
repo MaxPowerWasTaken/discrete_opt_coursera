a0ce72fe200dcd5346a8316ff7455394f3e22cdb
Max Epstein <mepstein68@gmail.com>
2020-04-09 21:19:03 -0400

    added code to consider distance back to starting node in look ahead when reach end of path, but still somehow worse than greedy

--- tsp/solver.py
+++ tsp/solver.py
@@ -75,28 +75,26 @@ def heuristic_search_salesman(distance_matrix,
         pp = PrettyPrinter(indent=4)
         for level in range(1, n_levels):
             for candidate in nodes_by_tree_lvl[level-1]:
-                if verbose:
-                    pp.pprint(nodes_by_tree_lvl)
-                    print('\n--------------------------------------------------------')
-                    print(f"calculating for level {level} and candidate {candidate}")
-                    print('--------------------------------------------------------')
-                    print(f"visited_already is: {visit_order}\n")
+                #if verbose:
+                    #pp.pprint(nodes_by_tree_lvl)
+                    #print('\n--------------------------------------------------------')
+                #    print(f"calculating for level {level} and candidate {candidate}")
+                    #print('--------------------------------------------------------')
+                    #print(f"visited_already is: {visit_order}\n")
                 candidate_ancestors = [int(a.name) for a in candidate.ancestors]
                 exclude = unique_l(visit_order + candidate_ancestors)
-                if verbose:
-                    print(f"exclude-list for next candidates is: {exclude}")
+                #if verbose:
+                #    print(f"exclude-list for next candidates is: {exclude}")
 
                 next_candidates = get_closest_nodes(int(candidate.name), distance_matrix, n_closest, exclude=exclude)
                 if verbose:
                     print(f"next candidates: {next_candidates}")
                     #print(RenderTree(root))
-
-                if debug:
-                    input("Press Enter to continue...")
-
+                #if debug:
+                    #input("Press Enter to continue...")
                 # Add new candidate nodes to next level of heuristic search tree
                 nodes_by_tree_lvl[level] = nodes_by_tree_lvl[level] + [Node(str(nc), parent=candidate) for nc in next_candidates]
-                print(RenderTree(root))
+                #print(RenderTree(root))
         # Now that the heuristic search tree is constructed, calculate full distance for each path,
         # next-step will be first-step along shortest planned path in search tree
         next_step = np.nan
@@ -104,13 +102,27 @@ def heuristic_search_salesman(distance_matrix,
         for possible_path in root.leaves:  #root.leaves looks like: (Node('/0/1/3'), Node('/0/1/4'), Node('/0/2/5'), Node('/0/2/6'))
             nodes = [n.name for n in possible_path.ancestors] + [possible_path.name]
             dist = sum(distance_matrix[int(i),int(j)] for i,j in zip(nodes[0:-1],nodes[1:]))
-            if verbose:
-                print(f"distance for {nodes} is {dist}")
+            #print(nodes)
+            #print(f"len of visit-order + nodes is {len(visit_order) + len(nodes)-1}, compared to {distance_matrix.shape[0]}")
+
+            # nodes includes prospective candidate paths, but also current node which is last item in visit order
+            if len(visit_order) + len(nodes)-1 == distance_matrix.shape[0]:
+                print(f"adding {distance_matrix[int(nodes[-1]), startnode]} as dist from {nodes[-1]} back to {startnode} to path {nodes}")
+                distance_back_to_start = distance_matrix[startnode, int(nodes[-1])]
+                dist = dist + distance_back_to_start
+                if debug:
+                    input("Press Enter to continue...")
+            
+            # HERE IS WHERE I SHOULD CONDITIONALLY UPDATE DIST TO INCLUDE LAST BACK TO FIRST (IF PATH IS LEN OF ALL POINTS)
+
+
+            #if verbose:
+            #    print(f"distance for {nodes} is {dist}")
             if dist < shortest_dist:
                 shortest_dist = dist
                 next_step = int(nodes[1])  # nodes[1] is second item in list, but first item is current-point. so nodes[1] is next step
 
-        print(f"next_step is: {next_step}")
+        #print(f"next_step is: {next_step}")
         visit_order.append(next_step)
 
     return visit_order
