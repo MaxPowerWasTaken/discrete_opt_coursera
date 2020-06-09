95f330cd8244a02f1eb54fa21be49bb0f495ba11
Max Epstein <mepstein68@gmail.com>
2020-06-09 18:27:25 -0400

    tsp.jl now takes input file via cli using ArgMacros, which I like better than julias argParse

--- tsp/tsp.jl
+++ tsp/tsp.jl
@@ -1,4 +1,4 @@
-using ArgParse
+using ArgMacros
 using DelimitedFiles # :readdlm   # CSVFiles
 using IterTools
 
@@ -31,9 +31,9 @@ function greedy_salesman(dist_matrix, startnode = 1)
     tour = [startnode]
     numpoints = size(dist_matrix)[1]
 
-    for i in 1:numpoints - 1  # we don't need to choose a 'next point' for our last point
+    for i in 1:numpoints - 1  # -1 bc we don't need to choose a 'next point' for our last point
         current_pt = tour[end]
-        distance_to_alternatives = dist_matrix[i,:]
+        distance_to_alternatives = dist_matrix[current_pt,:]
         # dont wanna select a point we've already visited
         distance_to_alternatives[tour] .= Inf
         # greedy - pt with min dist among possible choices
@@ -107,16 +107,14 @@ end
 function main()
 
     # Input File
-    INPUT_FILE = "data/tsp_51_1" # "data/for_jl_solver" #
-    points = readdlm(INPUT_FILE, ' ', Float64, skipstart = 1)
-
+    @beginarguments begin
+        @positionalrequired String input_file "input_file"
+        @argtest input_file isfile "The input must be a valid file" # Confirm that the input file really exists
+    end
+    points = readdlm(input_file, ' ', Float64, skipstart = 1)
 
     # Calc distance matrix and generate initial (greedy) tour
     dist_matrix = arr_points_to_dist_matrix(points)
-    display(dist_matrix)
-    writedlm("dist_matrix_jl.csv", dist_matrix, ',')
-
-
     tour = greedy_salesman(dist_matrix)
     println("Initial greedy tour: $tour")
     println("Initial greedy tour total dist: $(calc_tour_dist(tour, dist_matrix))")
@@ -132,7 +130,7 @@ function main()
             # println("Hit local optimum after $n moves")
         else
             tour, dist_improved = local_move
-            # println("Improved tour distance by $dist_improved")
+            println("Improved tour distance by $dist_improved on move $n")
         end
         n += 1
     end
@@ -151,25 +149,17 @@ end
 
 main()
 
-# BUG: why is tour I'm submitting worse than original greedy (should be 506)?
-# ...NOTE: says initial greedy tour dist is 1313...
-# TODO: check what initial greedy tour python code comes up with (need to convert 1-idx to 0-idx)
-# Julia greedy  starts: [0, 33, 1,  (after converting to 0-indexed)
-# Python greedy starts: [0, 33, 5,
-# so third step, python goes from 33 to 5, julia goes from (0-idxd) 33 to 1
-
-# TODO: compare dist matrices between julia and python code
-#   for tsp_5_1 RESULTS - same dist matrix but python one has more decimal precision
-#               UPDATE: same precision, can see when write to csv. display(m) in jl is just rounding
-#   for $prob1
-#     UPDATE: looks like a bug in jl greedy salesman. from 33, chooses 1, 14.036 away.
-#                                                              but 5, just like in py dist matrix, is closer in jl dist matrix (both say its 9.20 away)
-
-
 # TODO
-    # 7) call it from python with subprocess
-    # 8) submit
+    # call julia from solver.py passing along input datafile arg
+        # implement cli arg from julia side on input data path
+
+    # add time-limit - 29 minutes per problem - return current tour at that point
     # 9) add three-opt
+    #    add simulated annealing?
+    #    add simultaneous processing from different starting places?
+    #    show log output somewhere while running?
+    #       # maybe best to just do that by having logging statements so can see log when call
+    #       tsp.jl directly from shell, but those extra logging off by default when python/submit calls it
 
     # DONE
 # 1) get points from csv (skipping first line)
@@ -180,6 +170,8 @@ main()
 # 4) two_opt(tour, dist_matrix)
 #   - requires: dist_around(tour, nodeidx, dist_matrix)
     # 5) run it 
+    # 7) call it from python with subprocess
+    # 8) submit
 
 
 
